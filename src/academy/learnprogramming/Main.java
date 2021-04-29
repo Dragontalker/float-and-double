24b93d216d306594427e843f5f285fe3681da5ab
Richard <richard.yang.tong@gmail.com>
2021-04-28 20:53:15 -0400

    Added print example for float and double.

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -15,7 +15,10 @@ public class Main {
         System.out.println("Double Maximum Value = " + myMaxDoubleValue);
 
         int myIntValue = 5;
-        float myFloatValue = 5.25f;
-        double myDoubleValue = 5.25d;
+        float myFloatValue = 5f;
+        double myDoubleValue = 5d;
+        System.out.println("MyIntValue = " + myIntValue);
+        System.out.println("MyFloatValue = " + myFloatValue);
+        System.out.println("MyDoubleValue = " + myDoubleValue);
     }
 }
