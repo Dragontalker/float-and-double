22ea5ef935db56064f84db13c6d1bf1b6d0dd9bc
Richard <richard.yang.tong@gmail.com>
2021-04-28 20:47:49 -0400

    Added the range for double.

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -8,5 +8,10 @@ public class Main {
         float myMaxFloatValue = Float.MAX_VALUE;
         System.out.println("Float Minimum Value = " + myMinFloatValue);
         System.out.println("Float Maximum Value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double Minimum Value = " + myMinDoubleValue);
+        System.out.println("Double Maximum Value = " + myMaxDoubleValue);
     }
 }
